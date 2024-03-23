07321544947d9abfa914e52ede513b046e19476d
Artemis Levitt <artemis.levitt@gmail.com>
2024-03-23 11:44:25 +0100

    Creating shaders and drawing triangle

--- Source/Renderer/Renderer.h
+++ Source/Renderer/Renderer.h
@@ -16,16 +16,32 @@ public:
 	Renderer(Window* pWindow);
 	~Renderer() = default;
 
+	void SetPipeline();
+
+	void ClearColor(XMFLOAT4 color);
+	void Draw(uint32_t indexCount);
+	void Present();
+
+	ComPtr<ID3D11Device> GetDevice() const { return mDevice; }
+	ComPtr<ID3D11DeviceContext> GetDeviceContext() const { return mDeviceContext; }
+
+private:
 	void CreateDevice();
 	void CreateSwapChain();
 	void CreateRenderTargetView();
 
-	void ClearColor(XMFLOAT4 color);
-	void Present();
-private:
+	void CreateShaders();
+	void CreateInputLayout();
+
 	Window* mWindow;
 	ComPtr<ID3D11Device> mDevice;
 	ComPtr<ID3D11DeviceContext> mDeviceContext;
 	ComPtr<IDXGISwapChain> mSwapChain;
 	ComPtr<ID3D11RenderTargetView> mRenderTargetView;
+
+	ComPtr<ID3D11VertexShader> mVertexShader;
+	ComPtr<ID3DBlob> mVertexShaderBlob;
+	ComPtr<ID3D11PixelShader> mPixelShader;
+
+	ComPtr<ID3D11InputLayout> mInputLayout;
 };
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,26 @@
 #include <Window/Window.h>
 #include <Renderer/Renderer.h>
 
+
+struct VertexData
+{
+	XMFLOAT3 position;
+
+};
+
+VertexData vertices[] =
+{
+	{ XMFLOAT3(-0.5f, -0.5f, 0.0f) },
+	{ XMFLOAT3(0.0f, 0.5f, 0.0f)   },
+	{ XMFLOAT3(0.5f, -0.5f, 0.0f)  }
+
+};
+
+uint32_t indices[] =
+{
+	0, 1, 2
+};
+
 int main()
 {
 	Window* basicWindow = new Window({ 1280, 720 }, "D3D Window");
@@ -9,10 +29,48 @@ int main()
 
 	Renderer* basicRenderer = new Renderer(basicWindow);
 
+	ComPtr<ID3D11Buffer> vertexBuffer;
+	D3D11_BUFFER_DESC vertexBufferDesc = {};
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(VertexData) * 3;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA vertexSubresourceData = {};
+	vertexSubresourceData.pSysMem = vertices;
+	vertexSubresourceData.SysMemPitch = 0;
+	vertexSubresourceData.SysMemSlicePitch = 0;
+
+	basicRenderer->GetDevice()->CreateBuffer(&vertexBufferDesc, &vertexSubresourceData, vertexBuffer.GetAddressOf());
+
+	ComPtr<ID3D11Buffer> indexBuffer;
+	D3D11_BUFFER_DESC indexBufferDesc = {};
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(uint32_t) * 3;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA indexSubresourceData = {};
+	indexSubresourceData.pSysMem = indices;
+	indexSubresourceData.SysMemPitch = 0;
+	indexSubresourceData.SysMemSlicePitch = 0;
+
+	basicRenderer->GetDevice()->CreateBuffer(&indexBufferDesc, &indexSubresourceData, indexBuffer.GetAddressOf());
+
+	uint32_t stride = sizeof(VertexData);
+	uint32_t offset = 0;
+
+	basicRenderer->GetDeviceContext()->IASetVertexBuffers(0, 1, vertexBuffer.GetAddressOf(), &stride, &offset);
+	basicRenderer->GetDeviceContext()->IASetIndexBuffer(indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
+
 	while (basicWindow->isVisible())
 	{
 		basicWindow->Run();
 		basicRenderer->ClearColor({ 0.6f, 0.0f, 0.2f, 0.3f });
+		basicRenderer->SetPipeline();
+		basicRenderer->Draw(3);
 		basicRenderer->Present();
 	}
 
